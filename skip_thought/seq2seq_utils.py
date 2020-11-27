56674ecd72be6f0d72c0857634a9ad3a68acf4fc
lixiang.2533 <lixiang.2533@bytedance.com>
2020-11-27 08:25:37 +0800

    add dropout wrapper for multi-rnn

--- skip_thought/seq2seq_utils.py
+++ skip_thought/seq2seq_utils.py
@@ -5,17 +5,14 @@ import tensorflow as tf
 SEQ_LOSS_OUTPUT = namedtuple('SEQ_LOSS_OUTPUT', ['loss_id', 'loss_scaler', 'loss_per_time', 'loss_per_batch'])
 SEQ_PRED_OUTPUT = namedtuple('SEQ_PRED_OUTPUT', ['predict_prob', 'predict_id', 'predict_tokens', 'seq_len', 'vector'])
 ENCODER_FAMILY = {}
-DECODER_FAMILY = {}
 
 
 def encoder_decoder_collection(func_name):
     def helper(func):
         if 'encoder' in func_name:
             ENCODER_FAMILY[func_name] = func
-        elif 'decoder' in func_name:
-            DECODER_FAMILY[func_name] = func
         else:
-            raise Exception('Only encoder and decoder are supported in func_name')
+            raise Exception('Only encoder are supported in func_name')
         return func
     return helper
 
@@ -31,7 +28,9 @@ def build_rnn_cell(cell_type, params):
         raise Exception('Only rnn, gru, lstm are supported as cell_type')
 
     return tf.nn.rnn_cell.MultiRNNCell(
-        cells = [ cell_class(num_units = params['hidden_units'][i]) for i in range(params['cell_size']) ]
+        cells = [ tf.nn.rnn_cell.DropoutWrapper(cell = cell_class(num_units = params['hidden_units'][i]),
+                                                output_keep_prob=params['keep_prob'][i],
+                                                state_keep_prob= params['keep_prob'][i]) for i in range(params['cell_size']) ]
     )
 
 
